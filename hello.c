daa73379f512dda8341ec02921a64e232f015ab2
taeuk14 <ytaeuk@naver.com>
2017-10-14 20:24:50 +0900

    second commit

--- hello.c
+++ hello.c
@@ -1,15 +1,29 @@
 #include <stdio.h>
 
-int main()
-    int i;
-    double j;
-    char *bug =NULL;
+void check_num(int *lotto, int *input);
 
-    for(i=0; i<5; i++) {
-        j=i/2+i;
-        printf("j is %lf \n", j);
-    }
-    strcpy(bug, "hi");
-    printf("bug is %s \n", bug);
+int main(){
+    
+    int lotto_num[6] = {1, 2, 3, 10, 20, 30};
+    int input[6];
+
+    printf("input: ");
+    scanf("%d%d%d%d%d%d", &input[0], &input[1], &input[2], &input[3], &input[4], &input[5]);
+
+   check_num(lotto_num, input);
+    
+    return 0;
 }
+void check_num(int *lotto, int *input){
+    
+    int i,j;
+    int count=0;
 
+    for(i=0; i<6; i++){
+        for(j=0; j<i+1; j++)
+            if(lotto[j] == input[i])
+                count++;
+    }
+    printf("same lottonum:%d \n",count);
+}
+        
